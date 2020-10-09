59a32ad10977c79f6ed9e276ca861f51b37f03ca
vjack <1536520127@qq.com>
2020-10-09 10:42:26 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'StaticPagesController@home');//主页
+Route::get('/help', 'StaticPagesController@help');//帮助页
+Route::get('/about', 'StaticPagesController@about');//关于页
