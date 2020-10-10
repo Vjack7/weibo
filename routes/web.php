01128becb8af1a838fb8504d3d7d75348a42dc93
vjack <1536520127@qq.com>
2020-10-10 06:59:49 +0000

    用户注册界面

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');//主页
 Route::get('/help', 'StaticPagesController@help')->name('help');//帮助页
 Route::get('/about', 'StaticPagesController@about')->name('about');//关于页
+
+Route::get('signup', 'UsersController@create')->name('signup');
+
