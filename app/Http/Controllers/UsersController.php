b4808e32d408e1ef68dfed1ab06f9e848ede6600
vjack <1536520127@qq.com>
2020-10-14 05:59:56 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,6 +34,35 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);//约定优于配置，这的$user相当于$user->id
-
     }
+    //用户编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+    //用户提交
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+//        V1
+//        $user->update([
+//            'name' => $request->name,
+//            'password' => bcrypt($request->password)
+//        ]);
+//        V2
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功');
+        return redirect()->route('users.show', $user);
+    }
+
+
 }
